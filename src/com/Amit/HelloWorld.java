7cca2eb22c3d98d29c7b95d424bb18cad922b2d7
amit_qlogy <amit.j.joglekar@gmail.com>
2013-11-14 18:31:09 +0530

    jjjhhhjjkjkjkj

--- src/com/Amit/HelloWorld.java
+++ src/com/Amit/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public HelloWorld() {
         System.out.println("inside main constructor");
         System.out.println("another one");
-        System.out.println("hello ninad");
+
 
 
 
